9c481c5482b62135df116a858666668c78046e37
shoomar <shoomar@hotmail.com>
2021-06-02 15:29:41 +0200

    basic screen layout

--- main.js
+++ main.js
@@ -1,13 +1,128 @@
 "use strict";
+const contentDiv = document.getElementById('content');
+const availablePlayersDiv = document.getElementById('available-players');
+const optBtn = document.getElementById('optBtn');
+const addBtn = document.getElementById('addBtn');
+// options
+const optionsDiv = document.getElementById('options');
+const nameFormatInputs = document.getElementsByName('nameFormat');
+const resetBtn = document.getElementById('resetBtn');
+const backOptBtn = document.getElementById('backOptBtn');
+// all
+const allContainerDiv = document.getElementById('all-container');
+const allPlayersDiv = document.getElementById('all-players');
+const backPlyBtn = document.getElementById('backPlyBtn');
+var NameFormat;
+(function (NameFormat) {
+    NameFormat["fullName"] = "fullName";
+    NameFormat["camelCase"] = "camelCase";
+    NameFormat["name"] = "name";
+    NameFormat["surname"] = "surname";
+    NameFormat["nickname"] = "nickname";
+})(NameFormat || (NameFormat = {}));
+var Status;
+(function (Status) {
+    Status["available"] = "available";
+    Status["blue"] = "blue";
+    Status["red"] = "red";
+    Status["defeated"] = "defeated";
+    Status["off"] = "off";
+})(Status || (Status = {}));
 class Player {
     constructor(name, surname, nickname) {
         this.name = name;
         this.surname = surname;
         this.nickname = nickname;
+        this.idx_ = -1;
         this.fullName = `${name} ${surname}`;
         this.camelCase = name[0].toLowerCase() + surname;
+        this.status = Status.off;
+    }
+    get idx() {
+        return this.idx_;
+    }
+    setIdx(id) {
+        this.idx_ = id;
     }
 }
+function savePlayerPool() {
+    sessionStorage.setItem('playerPool', JSON.stringify(playerPool));
+}
+function initializePlayerPool() {
+    const playerPool = sessionStorage.getItem('playerPool');
+    if (playerPool) {
+        return JSON.parse(playerPool);
+    }
+    return [];
+}
+function initializeNameFormat() {
+    const nameFormat = localStorage.getItem('nameFormat');
+    if (nameFormat) {
+        return nameFormat;
+    }
+    localStorage.setItem('nameFormat', NameFormat.fullName);
+    return NameFormat.fullName;
+}
+function changeNameFormat(format) {
+    sortAndSavePlayers(format);
+    renderPlayers(format);
+    localStorage.setItem('nameFormat', format);
+}
+function sortAndSavePlayers(format = NameFormat.fullName) {
+    playerPool.sort((a, b) => a[format].localeCompare(b[format], locale));
+    playerPool.forEach((player, idx) => {
+        player.setIdx(idx);
+    });
+    savePlayerPool();
+}
+function addRemoveAvailablePlayers(e, ele, status) {
+    const target = e.target;
+    target.classList.add('none');
+    ele.childNodes.forEach((node) => {
+        const apd = node;
+        if (apd.id === target.id) {
+            apd.classList.remove('none');
+        }
+    });
+    playerPool[parseInt(target.id)].status = status;
+    savePlayerPool();
+}
+function renderPlayers(nameFormat = NameFormat.fullName) {
+    while (allPlayersDiv.lastChild) {
+        allPlayersDiv.lastChild.remove();
+        if (availablePlayersDiv.lastChild) {
+            availablePlayersDiv.lastChild.remove();
+        }
+    }
+    playerPool.forEach((player) => {
+        const playerDiv = document.createElement('div');
+        const availableDiv = document.createElement('div');
+        playerDiv.id = player.idx.toString();
+        playerDiv.classList.add('player');
+        playerDiv.innerText = player[nameFormat];
+        playerDiv.addEventListener('click', (e) => addRemoveAvailablePlayers(e, availablePlayersDiv, Status.available));
+        availableDiv.id = player.idx.toString();
+        availableDiv.classList.add('player');
+        availableDiv.classList.add('available');
+        availableDiv.innerText = player[nameFormat];
+        availableDiv.addEventListener('click', (e) => addRemoveAvailablePlayers(e, allPlayersDiv, Status.off));
+        switch (player.status) {
+            case Status.off:
+                availableDiv.classList.add('none');
+                playerDiv.classList.remove('none');
+                break;
+            case Status.available:
+                availableDiv.classList.remove('none');
+                playerDiv.classList.add('none');
+                break;
+            default:
+                break;
+        }
+        allPlayersDiv.appendChild(playerDiv);
+        availablePlayersDiv.appendChild(availableDiv);
+    });
+}
+const locale = 'sr';
 const pesCrew = [
     [
         'Aca',
@@ -120,44 +235,42 @@ const pesCrew = [
         'Vuk'
     ],
 ];
-const playerPool = [];
-pesCrew.forEach((crewMember) => {
-    playerPool.push(new Player(...crewMember));
-});
-function setNameFormat(format) {
-    localStorage.setItem('nameFormat', format);
-}
-function getNameFormat() {
-    return localStorage.getItem('nameFormat');
-}
-if (!getNameFormat())
-    setNameFormat('camelCase');
-const allPlayersDiv = document.getElementById('all-players');
-const availablePlayersDiv = document.getElementById('available-players');
-function renderPlayers(playerPool, nameFormat, nodeAll, nodeAvailable) {
-    playerPool.forEach((player) => {
-        const playerDiv = document.createElement('div');
-        playerDiv.id = player.camelCase;
-        playerDiv.classList.add('player');
-        playerDiv.innerText = player[nameFormat];
-        nodeAll.appendChild(playerDiv);
-        const availableDiv = document.createElement('div');
-        availableDiv.id = `x${player.camelCase}`;
-        availableDiv.classList.add('available');
-        availableDiv.innerText = player[nameFormat];
-        nodeAvailable.appendChild(availableDiv);
+const playerPool = initializePlayerPool();
+const nameFormat = initializeNameFormat();
+if (!playerPool.length) {
+    pesCrew.forEach((crewMember) => {
+        const player = new Player(...crewMember);
+        playerPool.push(player);
     });
+    sortAndSavePlayers();
+    renderPlayers();
 }
-function clearPlayers(node) {
-    while (node.lastChild) {
-        node.lastChild.remove();
+else
+    renderPlayers(nameFormat);
+optBtn.addEventListener('click', () => {
+    optionsDiv.classList.add('open');
+});
+addBtn.addEventListener('click', () => {
+    allContainerDiv.classList.add('open');
+});
+// options
+nameFormatInputs.forEach((input) => {
+    if (input.id === nameFormat) {
+        input.checked = true;
     }
-}
-renderPlayers(playerPool, localStorage.getItem('nameFormat'), allPlayersDiv, availablePlayersDiv);
-const removveButton = document.getElementById('remove');
-console.log(removveButton, 'dudu');
-removveButton === null || removveButton === void 0 ? void 0 : removveButton.addEventListener('click', () => {
-    clearPlayers(availablePlayersDiv);
-    clearPlayers(allPlayersDiv);
-    renderPlayers(playerPool, 'fullName', allPlayersDiv, availablePlayersDiv);
+});
+nameFormatInputs.forEach((input) => {
+    input.addEventListener('change', () => changeNameFormat(input.value));
+});
+resetBtn.addEventListener('click', () => {
+    localStorage.clear();
+    sessionStorage.clear();
+    window.location.reload();
+});
+backOptBtn.addEventListener('click', () => {
+    optionsDiv.classList.remove('open');
+});
+//all
+backPlyBtn === null || backPlyBtn === void 0 ? void 0 : backPlyBtn.addEventListener('click', () => {
+    allContainerDiv.classList.remove('open');
 });
--- main.ts
+++ main.ts
@@ -1,17 +1,156 @@
-class Player {
-	fullName: string;
-	camelCase: string;
+const contentDiv = document.getElementById('content') as HTMLDivElement;
+const availablePlayersDiv = document.getElementById('available-players') as HTMLDivElement;
+const optBtn = document.getElementById('optBtn') as HTMLButtonElement;
+const addBtn = document.getElementById('addBtn') as HTMLButtonElement;
+
+// options
+const optionsDiv = document.getElementById('options') as HTMLDivElement;
+const nameFormatInputs = document.getElementsByName('nameFormat') as NodeListOf<HTMLInputElement>;
+const resetBtn = document.getElementById('resetBtn') as HTMLButtonElement;
+const backOptBtn = document.getElementById('backOptBtn') as HTMLButtonElement;
+
+// all
+const allContainerDiv = document.getElementById('all-container') as HTMLDivElement;
+const allPlayersDiv = document.getElementById('all-players') as HTMLDivElement;
+const backPlyBtn = document.getElementById('backPlyBtn');
+
+
+enum NameFormat {
+	fullName = 'fullName',
+	camelCase = 'camelCase',
+	name = 'name',
+	surname = 'surname',
+	nickname = 'nickname'
+}
+type NamingConventions = {[key in NameFormat]: string};
+enum Status {
+	available = 'available',
+	blue = 'blue',
+	red = 'red',
+	defeated = 'defeated',
+	off = 'off'
+}
+
+class Player implements NamingConventions {
+	private idx_: number;
+	readonly fullName: string;
+	readonly camelCase: string;
+	status: Status;
 
 	constructor(
-		public name: string,
-		public surname: string,
-		public nickname: string,
+		readonly name: string,
+		readonly surname: string,
+		readonly nickname: string,
 	) {
+		this.idx_ = -1;
 		this.fullName = `${name} ${surname}`;
 		this.camelCase = name[0].toLowerCase() + surname;
+		this.status = Status.off;
+	}
+
+	get idx() {
+		return this.idx_;
+	}
+
+	setIdx(id: number) {
+		this.idx_ = id;
 	}
 }
 
+
+function savePlayerPool() {
+	sessionStorage.setItem('playerPool', JSON.stringify(playerPool));
+}
+
+function initializePlayerPool(): Player[] {
+	const playerPool = sessionStorage.getItem('playerPool');
+	if (playerPool) {
+		return JSON.parse(playerPool) as Player[];
+	}
+	return [];
+}
+
+function initializeNameFormat() {
+	const nameFormat = localStorage.getItem('nameFormat') as NameFormat;
+	if (nameFormat) {
+		return nameFormat;
+	}
+	localStorage.setItem('nameFormat', NameFormat.fullName);
+	return NameFormat.fullName;
+}
+
+function changeNameFormat(format: NameFormat) {
+	sortAndSavePlayers(format);
+	renderPlayers(format);
+	localStorage.setItem('nameFormat', format);
+}
+
+function sortAndSavePlayers(format = NameFormat.fullName) {
+	playerPool.sort((a, b) => a[format].localeCompare(b[format], locale));
+	playerPool.forEach((player, idx) => {
+		player.setIdx(idx);
+	});
+	savePlayerPool();
+}
+
+function addRemoveAvailablePlayers(e: MouseEvent, ele: HTMLDivElement, status: Status) {
+	const target = e.target as HTMLDivElement;
+	target.classList.add('none');
+	ele.childNodes.forEach((node) => {
+		const apd = node as HTMLDivElement;
+		if (apd.id === target.id) {
+			apd.classList.remove('none');
+		}
+	});
+	playerPool[parseInt(target.id)].status = status;
+	savePlayerPool();
+}
+
+function renderPlayers(
+	nameFormat = NameFormat.fullName
+) {
+	while (allPlayersDiv.lastChild) {
+		allPlayersDiv.lastChild.remove();
+		if (availablePlayersDiv.lastChild) {
+			availablePlayersDiv.lastChild.remove();
+		}
+	}
+	playerPool.forEach((player) => {
+		const playerDiv = document.createElement('div');
+		const availableDiv = document.createElement('div');
+
+		playerDiv.id = player.idx.toString();
+		playerDiv.classList.add('player');
+		playerDiv.innerText = player[nameFormat];
+		playerDiv.addEventListener('click', (e) => addRemoveAvailablePlayers(e, availablePlayersDiv, Status.available));
+
+		availableDiv.id = player.idx.toString();
+		availableDiv.classList.add('player');
+		availableDiv.classList.add('available');
+		availableDiv.innerText = player[nameFormat];
+		availableDiv.addEventListener('click', (e) => addRemoveAvailablePlayers(e, allPlayersDiv, Status.off));
+
+		switch (player.status) {
+			case Status.off:
+				availableDiv.classList.add('none');
+				playerDiv.classList.remove('none');
+				break;
+			case Status.available:
+				availableDiv.classList.remove('none');
+				playerDiv.classList.add('none');
+				break;
+			default:
+				break;
+		}
+
+		allPlayersDiv.appendChild(playerDiv);
+		availablePlayersDiv.appendChild(availableDiv);
+	});
+
+}
+
+
+const locale = 'sr';
 const pesCrew: ConstructorParameters<typeof Player>[] = [
 	[
 		'Aca',
@@ -125,65 +264,53 @@ const pesCrew: ConstructorParameters<typeof Player>[] = [
 	],
 ];
 
-const playerPool: Player[] = [];
-pesCrew.forEach((crewMember) => {
-	playerPool.push(new Player(...crewMember));
+
+
+const playerPool = initializePlayerPool();
+const nameFormat = initializeNameFormat();
+
+if (!playerPool.length) {
+	pesCrew.forEach((crewMember) => {
+		const player = new Player(...crewMember);
+		playerPool.push(player);
+	});
+	sortAndSavePlayers();
+	renderPlayers();
+}
+else renderPlayers(nameFormat);
+
+
+optBtn.addEventListener('click', () => {
+	optionsDiv.classList.add('open');
+});
+
+addBtn.addEventListener('click', () => {
+	allContainerDiv.classList.add('open');
 });
 
 
-
-function setNameFormat(format: keyof Player) {
-	localStorage.setItem('nameFormat', format);
-}
-
-function getNameFormat() {
-	return localStorage.getItem('nameFormat');
-}
-
-if (!getNameFormat()) setNameFormat('camelCase');
-
-
-
-const allPlayersDiv = document.getElementById('all-players') as HTMLDivElement;
-const availablePlayersDiv = document.getElementById('available-players') as HTMLDivElement;
-
-
-
-function renderPlayers(
-	playerPool: Player[],
-	nameFormat: keyof Player,
-	nodeAll: HTMLDivElement,
-	nodeAvailable: HTMLDivElement
-) {
-	playerPool.forEach((player) => {
-		const playerDiv = document.createElement('div');
-		playerDiv.id = player.camelCase;
-		playerDiv.classList.add('player');
-		playerDiv.innerText = player[nameFormat];
-		nodeAll.appendChild(playerDiv);
-		const availableDiv = document.createElement('div');
-		availableDiv.id = `x${player.camelCase}`;
-		availableDiv.classList.add('available');
-		availableDiv.innerText = player[nameFormat];
-		nodeAvailable.appendChild(availableDiv);
-	});
-
-}
-
-function clearPlayers(node: HTMLDivElement) {
-	while (node.lastChild) {
-		node.lastChild.remove();
-
+// options
+nameFormatInputs.forEach((input) => {
+	if (input.id === nameFormat) {
+		input.checked = true;
 	}
-}
+});
 
-renderPlayers(playerPool, localStorage.getItem('nameFormat') as keyof Player, allPlayersDiv, availablePlayersDiv);
+nameFormatInputs.forEach((input) => {
+	input.addEventListener('change', () => changeNameFormat(input.value as NameFormat));
+});
 
+resetBtn.addEventListener('click', () => {
+	localStorage.clear();
+	sessionStorage.clear();
+	window.location.reload();
+});
 
-const removveButton = document.getElementById('remove');
-console.log(removveButton, 'dudu');
-removveButton?.addEventListener('click', () => {
-	clearPlayers(availablePlayersDiv);
-	clearPlayers(allPlayersDiv);
-	renderPlayers(playerPool, 'fullName', allPlayersDiv, availablePlayersDiv);
+backOptBtn.addEventListener('click', () => {
+	optionsDiv.classList.remove('open');
+});
+
+//all
+backPlyBtn?.addEventListener('click', () => {
+	allContainerDiv.classList.remove('open');
 });
